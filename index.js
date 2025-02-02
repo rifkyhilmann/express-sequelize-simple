c36c340f22e63bdd229b256e19c911224810548a
Rifky Hilman <rifkyhilmanudin965@gmail.com>
2025-02-03 06:49:38 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const PORT = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.listen(PORT, function () {
+    console.log('Server is running on Port:', PORT);
+});
+
